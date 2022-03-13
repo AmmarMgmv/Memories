9ca73da9f14e2c77e4f5f08bd88d84e125f09769
AmmarMgmv <ammarmgmv@gmail.com>
2022-03-13 11:07:46 +0000

    Routing setup

--- Server/index.js
+++ Server/index.js
@@ -3,13 +3,17 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
 
-const CONNECTION_URL = 'my url should be here but for security reasons, it isnt';
+const CONNECTION_URL = 'mongodb+srv://Ammar:ammar123@cluster0.bnhmr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewURLParser: true, useUnifiedTopology: true})
