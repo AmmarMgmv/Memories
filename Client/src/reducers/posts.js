a3a0418df3b87911848ebd47188fefdc5380017b
AmmarMgmv <ammarmgmv@gmail.com>
2022-05-16 21:42:24 +0100

    Added redux capabilities, made api call, fixed bug

---
+++ Client/src/reducers/posts.js
@@ -0,0 +1,16 @@
+const reducer = (posts = [], action) => 
+{
+    switch (action.type) 
+    {
+        case 'CREATE':
+            return posts; 
+
+        case 'FETCH_ALL':
+            return action.payload;
+
+        default:
+            return posts;
+    }
+}
+
+export default reducer;
