f04837b9468c302dbb871948d53cbbe2a9573bb0
AmmarMgmv <ammarmgmv@gmail.com>
2023-04-09 16:57:09 +0100

    Added function to get a users friend list and their data

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -14,3 +14,28 @@ export const getUser = async (req, res) => {
         res.status(404).json({message: err.message});
     }
 }
+
+//This function gets a users friend list based on the provided ID
+export const getUserFriends = async (req, res) => {
+    try{
+        //Grab ID from the request
+        const {id} =req.params;
+        //Search database to find the user with this ID
+        const user = await User.findById(id);
+
+        //Grab all user data for all the friend ID's
+        const friends = await Promise.all(
+            user.friends.map((id) => User.findById(id))
+        );
+        //Formats friend array to include relevant data only
+        const formattedFriends = friends.map(
+            ({_id, firstName, lastName, occupation, location, picturePath}) => {
+                return {_id, firstName, lastName, occupation, location, picturePath};
+            }
+        );
+        res.status(200).json(formattedFriends);
+    }
+    catch (err) {
+        res.status(404).json({message: err.message});
+    }
+}
