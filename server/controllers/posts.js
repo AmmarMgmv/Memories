77cfb6bd39c6bdabb84d28db3643bd4465dca338
AmmarMgmv <ammarmgmv@gmail.com>
2023-04-09 19:06:28 +0100

    Added function to create new post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,1 +1,34 @@
 import Post from "../models/Post.js";
+import User from "../models/User.js";
+
+//CREATE
+export const createPost = async (req, res) => {
+    try {
+        //Grab userId, description and picturePath from the request
+        const {userId, description, picturePath} = req.body;
+        //Find the user with that ID
+        const user = await User.findById(userId);
+        //This creates a new instance of post with the extracted data from req & user data
+        const newPost = new Post({
+            userId, 
+            firstName: user.firstName,
+            lastName: user.lastName,
+            location: user.location,
+            description,
+            userPicturePath: user.picturePath,
+            picturePath,
+            likes: {},
+            comments: []
+        })
+        //This saves the new post to the database
+        await newPost.save();
+
+        //Send the updated list of post to the front end
+        const post = await Post.find();
+        res.status(201).json(post);
+    }
+    catch (err) {
+        res.status(409).json({message: err.message})
+    }
+}
+
