e9a52d738433884a47560027a254a91cb0a8bbdc
AmmarMgmv <ammarmgmv@gmail.com>
2023-04-10 01:52:29 +0100

    Added function to like and unlike posts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -37,9 +37,9 @@ export const createPost = async (req, res) => {
 //This function allows us to get the feed posts
 export const getFeedPosts =async (req, res) => {
     try {
-         //Send the updated list of post to the front end
-         const post = await Post.find();
-         res.status(200).json(post);
+        //Send the updated list of post to the front end
+        const post = await Post.find();
+        res.status(200).json(post);
     }
     catch (err) {
         res.status(409).json({message: err.message})
@@ -51,9 +51,45 @@ export const getUserPosts =async (req, res) => {
     try {
         //Grab userID from the req
         const {userId} = req.params;
-         //Send the posts that belong to the user 
-         const post = await Post.find({userId});
-         res.status(200).json(post);
+        //Send the posts that belong to the user 
+        const post = await Post.find({userId});
+        res.status(200).json(post);
+    }
+    catch (err) {
+        res.status(409).json({message: err.message})
+    }
+}
+
+//UPDATE
+//This function allows the user to like and unlike posts
+export const likePost = async (req, res) => {
+    try {
+        //Grab the id from the request
+        const {id} = req.params;
+        //Grab the userId from the body of the request
+        const {userId} = req.body;
+        //Find the post using its ID
+        const post = await Post.findById(id);
+        //Check if the userID is in the likes 
+        const isLiked = post.likes.get(userId);
+
+        //If its liked, clicking like will unlike it
+        if (isLiked) {
+            post.likes.delete(userId);
+        }
+        //If it isnt liked, clicking like will like it
+        else {
+            post.likes.set(userId, true);
+        }
+
+        //Get the post with the updated number of likes
+        const updatedPost = await Post.findByIdAndUpdate(
+            id,
+            {likes: post.likes},
+            {new: true}
+        );
+
+        res.status(200).json(updatedPost);
     }
     catch (err) {
         res.status(409).json({message: err.message})
