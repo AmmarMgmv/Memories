97c134ca5458bc784e9783adb604cbb8b76b05e0
AmmarMgmv <ammarmgmv@gmail.com>
2023-04-10 01:59:09 +0100

    Fixed errors

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,4 +1,4 @@
-import User from "../models/User";
+import User from "../models/User.js";
 
 //READ
 //This function gets the user data based on the provided ID
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,6 @@
-import { express } from "express";
+import express from "express";
 import {getUser, getUserFriends, addRemoveFriend, } from "../controllers/users.js";
-import {verifiedToken, verifyToken} from "../middleware/auth.js";
+import {verifyToken} from "../middleware/auth.js";
 
 const router = express.Router();
 
@@ -10,6 +10,7 @@ router.get("/:id", verifyToken, getUser);
 //Grabs the users friends
 router.get("/:id/friends", verifyToken, getUserFriends);
 
+//UPDATE
 //Updates friends list when fried is added/removed
 router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
