e15220453f65494b6fdfda5f3323fbb8e1470a03
AmmarMgmv <ammarmgmv@gmail.com>
2023-04-11 23:25:08 +0100

    Fixed error, made navbar visible

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Box, IconButton, InputBase, Typography, Select, MenuItem, FormControl, useTheme, useMediaQuery } from "@mui/material";
-import { Ssearch, Message, DarkMode, LightMode, Notifications, Help, Menu, Close, Search } from "@mui/icons-material"
+import { Search, Message, DarkMode, LightMode, Notifications, Help, Menu, Close } from "@mui/icons-material"
 import { useDispatch, useSelector } from "react-redux";
 import { setMode, setLogout } from "state";
 import { useNavigate } from "react-router-dom";
@@ -33,7 +33,7 @@ const Navbar = () => {
         <FlexBetween gap="1.75rem">
             <Typography 
                 fontWeight="bold" 
-                fontSize="clamp(1rem, 2rem, 2.25rem" 
+                fontSize="clamp(1rem, 2rem, 2.25rem)" 
                 color="primary" 
                 onClick={() => navigate("/home")}
                 sx={{
