bfd547b58c508c64098269faf14e9c2f16c24b8f
AmmarMgmv <ammarmgmv@gmail.com>
2023-04-12 15:12:36 +0100

    Added welcome message

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -18,6 +18,18 @@ const LoginPage = () => {
                 Memories
             </Typography>
         </Box>
+
+        <Box
+            width={isNonMobileScreens ? "50%" : "93%"}
+            p="2rem"
+            m="2rem auto"
+            borderRadius="1.5rem"
+            backgroundColor={theme.palette.background.alt}
+        >
+            <Typography fontWeight="600" variant="h5" sx={{mb: "1.5rem"}}>
+                Welcome to Memories, the place where you can share, store and cherish all of your favourite moments!
+            </Typography>
+        </Box>
     </Box>
 };
 
